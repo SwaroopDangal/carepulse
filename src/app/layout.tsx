d1dbb3f1654cb602a035410e0186325ba0ae0de4
SwaroopDangal <swaroopdangal732@gmail.com>
2026-01-12 12:18:04 +0545

    refactor: :white_check_mark: setup completed
    shadcn setup,tailwind css setup completed

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { cn } from "../lib/utils";
+import React from "react";
+import { ThemeProvider } from "../components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "carepulse",
+  description: "A healthcare management platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-white text-gray-200">click me</Button>
+    </div>
+  );
+}
