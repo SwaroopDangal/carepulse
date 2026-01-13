acb069e0be32c5a5ac40ea2b024b811581b21e58
SwaroopDangal <swaroopdangal732@gmail.com>
2026-01-13 11:06:36 +0545

    homepage form's fucntionality added

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -48,8 +49,8 @@ const PatientForm = () => {
         phone,
       };
 
-      // const user = await createUser(userData);
-      // if (user) router.push(`/patients/${user.$id}/register`);
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
